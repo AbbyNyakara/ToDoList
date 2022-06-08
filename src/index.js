4825fd7852943e9f17a5f8c0a005cb674292b486
Abby Nyakara <abbynyakara@gmail.com>
2022-06-08 13:02:23 +0300

    Add initial files

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import './index.css';
+
+const list = document.querySelector('.list');
+
+let listArray = [
+  {
+    description: 'Wash the dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Finish coding assignment',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Grocery shopping',
+    completed: false,
+    index: 2,
+  },
+];
+
+list.innerHTML = '';
+
+for (let i=0; i<listArray.length; i++) {
+  list.innerHTML += `
+  <div class="list-content">
+    <div class="left">
+      <input type="checkbox" name="" id="">
+      <p>${listArray[i].description}</p>
+    </div>
+    <div class="right">
+      <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+    </div>
+  </div>
+  `;
+}
