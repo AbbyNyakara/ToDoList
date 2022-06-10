0b6bba6dbbb42b974b8dddedae55ed9b9361edba
Abby Nyakara <abbynyakara@gmail.com>
2022-06-10 13:49:07 +0300

    add and delete functionality

--- src/index.js
+++ src/index.js
@@ -1,42 +1,117 @@
-/* eslint-disable no-plusplus */
-
 import './index.css';
 
+// Select the todo-list to be updated
 const list = document.querySelector('.list');
 
-const listArray = [
-  {
-    description: 'Wash the dishes',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Finish coding assignment',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Grocery shopping',
-    completed: false,
-    index: 2,
-  },
-];
+let id=0;
+let listArray = [] || JSON.parse(localStorage.getItem('list')); 
+const userInput = document.querySelector('.user-input');
 
-const displayToDo = () => {
-  list.innerHTML = '';
-  for (let i = 0; i < listArray.length; i++) {
-    list.innerHTML += `
-      <div class="list-content">
-        <div class="left">
-          <input type="checkbox" name="" id="">
-          <p>${listArray[i].description}</p>
-        </div>
-        <div class="right">
-          <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-        </div>
+// todo item
+let addtodo = (todo, id, completed, trash) => {   
+  if (trash) { return; }
+
+  const text = `
+  <div class="list-content">
+      <div class="left">
+        <i class="fa-solid fa-square check" id="${id}"></i>
+        <p class="text" contenteditable="true" >${todo}</p>
       </div>
+      <div class="right">
+        <i class="fa fa-ellipsis-v more" aria-hidden="true" id="${id}"></i>
+      </div>
+  </div>
   `;
-  }
-};
 
-displayToDo();
+  const position = 'beforeend';
+
+  list.insertAdjacentHTML(position, text);
+}
+
+
+// ************** Add event listener to listen for the enter key and add a new todo item //
+document.addEventListener('keyup', (event)=>{
+  if (event.key === 'Enter') {
+    const todo = userInput.value; 
+    if (todo) {
+      addtodo(todo, id, false, false);
+      listArray.push ({
+        description: todo,
+        completed: false,
+        id: id,
+        trash: false
+      });
+      
+      id ++
+      userInput.value = '';
+      // Local storage
+      localStorage.setItem('list', JSON.stringify(listArray)); 
+    } 
+    else {
+      alert ('Please add an item')
+    }
+  }
+});
+
+// **************** Functionality to detect click and delete *************//
+
+// Classes Names
+const checked = 'fa-square-checked';
+const bin = 'fa-trash-can'
+
+let detectClick = () => {
+  list.addEventListener('click', (e)=>{
+    // alert("clicked list");
+    const element = e.target;
+    const textInput = element.parentNode.lastElementChild;
+    const elipsis = element.parentNode.parentNode.lastElementChild.firstElementChild;
+
+    if (element.classList.contains('check') || element.classList.contains('text')) {
+      elipsis.classList.toggle('fa-ellipsis-v');
+      elipsis.classList.toggle('fa-trash-can');
+    }
+
+    if (element.classList.contains('check')){
+      if(element.classList.contains('fa-square')) {
+        element.classList.remove('fa-square');
+        element.classList.add('fa-square-check'); // The unchecked element
+      } else {
+        element.classList.add('fa-square');
+        element.classList.remove('fa-square-check'); 
+      }
+    }
+
+    if (element.classList.contains('fa-square-check')) {
+      textInput.classList.add('line-through');
+    } else {
+      textInput.classList.remove('line-through');
+    }
+
+    if(elipsis.classList.contains('fa-trash-can')){
+      const trashIcon = element.parentNode.parentNode.lastElementChild.firstElementChild;
+      const deleteTask = element.parentNode.parentNode;
+      trashIcon.onclick = ()=> {
+        listArray[element.id].trash = true;
+        listArray[element.id].completed = true;
+        deleteTask.remove();
+      }
+      // Filter the array if the trash = true;
+      listArray = listArray.filter(function( obj ) {
+        return obj.completed !== true;
+      });
+
+      // Update the indexes 
+      listArray = listArray.map((todo, index) => {
+        todo.id = index ;
+        return todo;
+      });
+
+      localStorage.setItem('list', JSON.stringify(listArray)); 
+    
+    } else {
+      return;
+    }
+  });
+}
+
+detectClick();
